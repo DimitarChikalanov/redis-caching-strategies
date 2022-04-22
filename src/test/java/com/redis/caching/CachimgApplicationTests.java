b5e88c28ec7dfe91afe5c8ca38ae679d5795b1e6
DimitarChikalanov <dimitur.chikalanov@gmail.com>
2022-04-22 15:54:34 +0300

    init

---
+++ src/test/java/com/redis/caching/CachimgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachimgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
